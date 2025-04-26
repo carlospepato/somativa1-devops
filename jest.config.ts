7221a7f0fefc66365a4ec259b4052949e2330d07
Carlos Pepato <kadubelber1@gmail.com>
2025-04-26 16:04:14 -0300

    feat: create test

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
+
+export default config;
